e932bf9a3243f626282191b3a5007fe477c9cfc1
Yash Kamath <yash.k@rogers.com>
2020-09-05 15:58:10 -0400

    Created Post/User Models

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('about/', views.about, name='about-home'),
+    path('about/', views.about, name='blog-about'),
 ]
---
+++ django_project/users/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def register(request):
+    # Use built-in form, to make it easier for user registration
+    # Create form here for templates
